4baca92d6f9692907002fec7fea0830f2584adf0
Aditya Kocheri <adityakocheri@MacBookPro.lan>
2025-10-28 23:24:11 +0530

    Update: Fixed the Google Logo

--- frontend_vite/src/pages/Login.jsx
+++ frontend_vite/src/pages/Login.jsx
@@ -685,7 +685,7 @@ export default function Login() {
                       display: "flex",
                       alignItems: "center",
                       justifyContent: "center",
-                      gap: 20,
+                      gap: "10px",
                     }}
                   >
                     <svg
